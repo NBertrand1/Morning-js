316ee1952b84d47a6a6ab800cb7d66c5529c3044
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-05 06:15:38 +0100

    Day 1 Js morning

---
+++ jour-1.js
@@ -0,0 +1,10 @@
+// variables de depart
+const prixHorsTaxe = 10000;
+const tauxTVA = 0.1925;
+
+// variables après calculs
+const TVA = prixHorsTaxe * tauxTVA;
+const prixTTC = prixHorsTaxe + TVA;
+
+//résultat 
+console.log(`Pour un prix de 10000 FCFA, la TVA est de ${TVA} FCFA. Total : ${prixTTC} FCFA.`)
