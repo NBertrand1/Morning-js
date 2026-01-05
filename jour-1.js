27154fd3e6c78e08be6c6349366048fcc0a7b1ee
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-05 06:22:01 +0100

    DAy 1 JS morning.

--- jour-1.js
+++ jour-1.js
@@ -1,10 +1,18 @@
-// variables de depart
-const prixHorsTaxe = 10000;
-const tauxTVA = 0.1925;
+/* variables de depart
+prix_TTC();
 
-// variables après calculs
-const TVA = prixHorsTaxe * tauxTVA;
-const prixTTC = prixHorsTaxe + TVA;
+function prix_TTC() {
+    const prixHorsTaxe = 10000;
+    const tauxTVA = 0.1925;
 
-//résultat 
-console.log(`Pour un prix de 10000 FCFA, la TVA est de ${TVA} FCFA. Total : ${prixTTC} FCFA.`)
+    // variables après calculs
+    const TVA = prixHorsTaxe * tauxTVA;
+    const prixTTC = prixHorsTaxe + TVA;
+
+    //résultat 
+    console.log(`Pour un prix de 10000 FCFA, la TVA est de ${TVA} FCFA. Total : ${prixTTC} FCFA.`);
+}*/ 
+
+const langage = "javascript";
+
+console.log(langage.slice(4, 10));
