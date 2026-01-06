0a932e13a81ddb226076b6e11ac09fb621078a29
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-06 06:59:38 +0100

    feat: implement FizzBuzz logic and activate previous exercises.

--- jour-2.js
+++ jour-2.js
@@ -1,16 +1,28 @@
-let age = 18;
-
 /*exercice 1 ;
 if (age >= 18) {
     console.log("Bienvenu(e)");
 } else {
     console.log("Accès refusé");
-}*/
+}
 
-/*exercice 2
+exercice 2
 for (let index = 0; index < age; index++) {
     console.log("Bonne année !");
-};*/
+};
 
-//exercice 3
+exercice 3
 age>=18?console.log("Bienvenu(e)"):console.log("Accès refusé");
+*/
+
+//fizzbuzz
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
