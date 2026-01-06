8118e57a5f708cd14ddf74a83abcfb7cdf855215
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-06 06:47:06 +0100

    feat: Wrap exercise 1 in a multi-line comment and implement exercise 2 with a for loop.

--- jour-2.js
+++ jour-2.js
@@ -1,9 +1,13 @@
-//exercice 1 ;
 let age = 18;
 
+/*exercice 1 ;
 if (age >= 18) {
     console.log("Bienvenu(e)");
 } else {
     console.log("Accès refusé");
-}
+}*/
 
+//exercice 2
+for (let index = 0; index < age; index++) {
+    console.log("Bonne année !");
+};
