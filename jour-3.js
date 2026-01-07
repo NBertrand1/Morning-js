25d7c8f8320c439669673cb00bee29bbc32a6c1e
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-07 05:17:35 +0100

    feat: add user profile object and personalized greeting

---
+++ jour-3.js
@@ -0,0 +1,8 @@
+let utilisateur = {
+    nom : "TANG ELOUMOU",
+    age : 20,
+    ville : "Yaoundé",
+    estConnecte : "True",
+}
+
+console.log(`Bonjour, je m'appelle ${utilisateur.nom} et j'habite à ${utilisateur.ville}.`);
