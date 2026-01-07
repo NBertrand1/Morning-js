22e82c5fe20482d9ee355a96a267e0c484452b65
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-07 05:30:15 +0100

    feat: add shopping cart total calculation and comment out user information display.

--- jour-3.js
+++ jour-3.js
@@ -1,3 +1,4 @@
+/*Exercice 1
 let utilisateur = {
     nom : "TANG ELOUMOU",
     age : 20,
@@ -7,3 +8,18 @@ let utilisateur = {
 }
 
 console.log(`Bonjour, je m'appelle ${utilisateur.nom} et j'habite à ${utilisateur.ville}. J'aime le ${utilisateur.hobbies.join(", ")}`);
+*/
+//Exercice 2
+let panier = [
+    { nom: "Pomme", prix: 2000, quantite: 5 },
+    { nom: "Orange", prix: 4000, quantite: 8 },
+    { nom: "Banane", prix: 1000, quantite: 3 }
+]
+
+let prixTotalPanier = 0 ; 
+
+panier.forEach(element => {
+    prixTotalPanier += element.prix * element.quantite;
+});
+
+console.log(`le prix total du panier est : ${prixTotalPanier} FCFA`);
