98f8cfa16ec3cd9b5dcbdc1bd527a7c23c8ffc8c
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-07 05:42:03 +0100

    feat: uncomment Exercice 1 and 2, and add Exercice 3 for age filtering.

--- jour-3.js
+++ jour-3.js
@@ -8,7 +8,7 @@ let utilisateur = {
 }
 
 console.log(`Bonjour, je m'appelle ${utilisateur.nom} et j'habite à ${utilisateur.ville}. J'aime le ${utilisateur.hobbies.join(", ")}`);
-*/
+
 //Exercice 2
 let panier = [
     { nom: "Pomme", prix: 2000, quantite: 5 },
@@ -23,3 +23,9 @@ panier.forEach(element => {
 });
 
 console.log(`le prix total du panier est : ${prixTotalPanier} FCFA`);
+*/
+//Exercice 3
+let agesArray = [ 12, 16, 29, 56, 39, 25, 19, 8, 30, 45, 60 ]; 
+let filteredAges = agesArray.filter(age => age >= 18);
+
+console.log(filteredAges);
