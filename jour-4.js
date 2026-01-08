835dbeb2fec6b24d15332fc1504d5b1e650d02ab
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-08 11:57:07 +0100

    feat: Implement counter exercise (Day 4 Ex 2)

--- jour-4.js
+++ jour-4.js
@@ -1,6 +1,36 @@
+/*Exercice 1
 const greetingName = document.getElementById("name");
 const greetingBtn = document.getElementById("greeting-button");
 
 greetingBtn.addEventListener("click", () => {
     alert(`Bonjour  ${greetingName.value}`);
+});*/
+
+//Exercice 2
+
+const counterElement = document.getElementById("counter");
+let counter = parseInt(counterElement.textContent);
+const plus = document.getElementById("plus");
+const moins = document.getElementById("moins");
+
+function updateDisplay() {
+    // Met à jour le texte dans le HTML
+    counterElement.textContent = counter; // ou .textContent
+
+    // Met à jour la couleur
+    if (counter > 0) {
+        counterElement.style.color = "green";
+    } else {
+        counterElement.style.color = "red";
+    }
+}
+
+plus.addEventListener("click", () => {
+    counter++; // Incrémente de 1
+    updateDisplay();
+});
+
+moins.addEventListener("click", () => {
+    counter--; // Décrémente de 1
+    updateDisplay();
 });
