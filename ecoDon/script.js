80796dd9868ffc890e93ed539b86a686b31e7f4e
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-12 22:05:02 +0100

    feat: link js file for dynamism and add some constants

--- ecoDon/script.js
+++ ecoDon/script.js
@@ -1,1 +1,6 @@
 const headerButton = document.getElementById("header-button");
+const headerText = document.querySelector(".header-text");
+const donationGift = document.getElementById("donation-name");
+const donationQuantity = document.getElementById("donation-quantity");
+const addDonationBtn = document.getElementById("add-donation-button");
+
