a1c2bdc1370bcc4e395065d660a0d5534e133a2e
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-15 05:26:28 +0100

    feat: Implement dark mode and refactor donation card styling and layout.

--- ecoDon/script.js
+++ ecoDon/script.js
@@ -11,16 +11,26 @@ const donationQuantity = document.getElementById("donation-quantity");
 const addDonationBtn = document.getElementById("add-donation-button");
 const donationList = document.getElementById("card-container");
 
+function darkMode() {
+    themeBtn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+}
+
+darkMode();
+
 function showDonationList(dons) {
     donationList.innerHTML = ""; // On vide la liste            
         dons.forEach(don => {                
             const card = document.createElement("div");
             card.innerHTML = `
-                 <div>
-                    <h3 class="font-bold text-lg">${don.nom}</h3>
-                    <p class="text-sm opacity-70">Quantité : ${don.quantite}</p>                    
+                 <div class="card">
+                    <div class="card-info">
+                        <h3 class="font-bold text-lg">${don.nom}</h3>
+                        <p class="text-sm opacity-70">Quantité : ${don.quantite}</p>
+                    </div>                    
+                    <button onclick="reserver(${don.id})" class="text-emerald-600 font-bold hover:underline">Réserver</button>
                 </div>
-                <button onclick="reserver(${don.id})" class="text-emerald-600 font-bold hover:underline">Réserver</button>
             `;
             donationList.appendChild(card);
     });
