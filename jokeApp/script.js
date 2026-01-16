9a0b240d5abc8c058db124dabce618c843c4ad23
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-16 06:12:45 +0100

    feat: Fetch a joke automatically on page load and horizontally center the button.

--- jokeApp/script.js
+++ jokeApp/script.js
@@ -1,9 +1,12 @@
-const url = "https://api.api-ninjas.com/v1/dadjokes";
 const jokeBtn = document.getElementById("joke-button");
 const jokeContent = document.getElementById("joke-content");
 
-async function jokeFetch(url) {
+async function jokeFetch() {
+    const url = "https://api.api-ninjas.com/v1/dadjokes";
     const response = await fetch(url);
     const data = await response.json();
+    
     console.log(data);
 }
+
+jokeFetch();
