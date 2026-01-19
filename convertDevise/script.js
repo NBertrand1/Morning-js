cd1257af6e587330197df0a4822b82792096e273
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-19 05:39:08 +0100

    feat: improve currency converter UI with dedicated currency symbols and enhanced styling.

--- convertDevise/script.js
+++ convertDevise/script.js
@@ -6,7 +6,7 @@ async function convertir() {
         const res = await fetch('https://api.exchangerate-api.com/v4/latest/EUR');
         const data = await res.json();
         const taux = data.rates.USD;
-        document.querySelector('#resultat').textContent = montant * taux + "$";
+        document.querySelector('#resultat').textContent = montant * taux;
     } catch (e) {
         console.error("Erreur de conversion");
     }
