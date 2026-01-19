8af7339e1bd37aa90a31706afb5c481cef5994a2
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-19 05:21:14 +0100

    feat: Implement a basic EUR to USD currency converter with HTML and JavaScript.

---
+++ convertDevise/script.js
@@ -0,0 +1,16 @@
+const convertBtn = document.querySelector('#convert-btn');
+
+async function convertir() {
+    const montant = document.querySelector('#montant').value;
+    try {
+        const res = await fetch('https://api.exchangerate-api.com/v4/latest/EUR');
+        const data = await res.json();
+        const taux = data.rates.USD;
+        document.querySelector('#resultat').textContent = montant * taux + "$";
+    } catch (e) {
+        console.error("Erreur de conversion");
+    }
+}
+
+convertBtn.addEventListener('click', convertir);
+
