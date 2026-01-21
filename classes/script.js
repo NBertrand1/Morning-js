00776c039d7f0e6ba4aac6ef0b0f83653f93c582
NBertrand1 <eloumounarcisse@gmail.com>
2026-01-21 06:26:23 +0100

    feat: decrement attacker's energy and update the display to show both attacker's energy and target's health.

--- classes/script.js
+++ classes/script.js
@@ -8,7 +8,8 @@ class Personnage{
     attaquer(personnage){
         console.log(`${this.nom} attaque ${personnage.nom}`);
         personnage.sante -= 10;
-        document.getElementById("affichage").innerHTML = `<p>${personnage.nom} a ${personnage.sante} points de vie`;
+        this.energie -= 10;
+        document.getElementById("affichage").innerHTML = `<p>${personnage.nom} a ${personnage.sante} points de vie </br> ${this.energie} points d'energie`;
         console.log(`${personnage.nom} a ${personnage.sante} points de vie`);
         if(personnage.sante <= 0){
             console.log(`${personnage.nom} est mort`);
